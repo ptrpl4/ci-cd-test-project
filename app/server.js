aa137d4cfd6d4a0050977e049ebf4d413654ae48
Petr V <ptrpl4@ya.ru>
2023-03-04 23:27:33 +0800

    changes to connect with docker

--- app/server.js
+++ app/server.js
@@ -1,18 +1,83 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  console.log("index");
+ }); 
+
+// use when starting application locally
+const mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+// use when starting application as docker container
+const mongoUrlDocker = "mongodb://admin:password@mongodb";
+
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+const databaseName = "my-db";
+
+
+app.get('/profile-picture', (req, res) => {
+  const img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+app.post('/update-profile', function (req, res) {
+  const userObj = req.body;
+
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    const db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    const myquery = { userid: 1 };
+    const newvalues = { $set: userObj };
+
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
+  // Send response
+  res.send(userObj);
+});
+
+app.get('/get-profile', function (req, res) {
+  const response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    const db = client.db(databaseName);
+
+    const myquery = { userid: 1 };
+
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
+
+      // Send response
+      res.send(response ? response : {});
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log("app listening on port 3000! http://localhost:3000/ ");
 });
