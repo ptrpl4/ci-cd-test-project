834dc1b8d74359c083db0b6159287f03d4326280
Petr V <ptrpl4@ya.ru>
2023-03-06 21:08:30 +0800

    remove unused comment

--- app/server.js
+++ app/server.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable no-shadow */
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -6,27 +10,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+// use when starting application locally
+const mongoUrlLocal = 'mongodb://admin:password@localhost:27017';
+// use when starting application as docker container
+const mongoUrlDocker = 'mongodb://admin:password@mongodb';
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const databaseName = 'user-account';
 
+// set up app
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/index.html`));
 });
-
-// use when starting application locally
-const mongoUrlLocal = 'mongodb://admin:password@localhost:27017';
-
-// use when starting application as docker container
-const mongoUrlDocker = 'mongodb://admin:password@mongodb';
-
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
-const databaseName = 'user-account';
 
 app.get('/profile-picture', (req, res) => {
   const img = fs.readFileSync(path.join(__dirname, 'images/profile-1.jpg'));
